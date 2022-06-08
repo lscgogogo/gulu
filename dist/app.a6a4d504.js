3d75917f366ede8bde89894e39e04d2cdc5d4f88
梁书聪 <8948760+liang-shucong@user.noreply.gitee.com>
2022-06-08 22:29:21 +0800

    更新package

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -26837,7 +26837,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50259" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61516" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
