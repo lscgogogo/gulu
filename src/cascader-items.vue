67829ee8a18eee58e8d6bb1a3dae472c932a6822
DESKTOP-0FDNEV4\yc <850925770@qq.com>
2022-10-20 14:53:05 +0800

    初步渲染

---
+++ src/cascader-items.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="cascaderItem" :style="{ height: height }">
+    <div class="left">
+      <div
+        class="label"
+        v-for="(item, index) in items"
+        :key="index"
+        @click="leftSelected = item"
+      >
+        {{ item.name }}
+        <icon class="icon" v-if="item.children" name="right"></icon>
+      </div>
+    </div>
+    <div class="right" v-if="rightItems">
+      <gulu-cascader-items :items="rightItems" :height="height" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from './icon'
+export default {
+  name: 'GuluCascaderItems',
+  components: {
+    Icon,
+  },
+  props: {
+    items: {
+      type: Array,
+    },
+    height: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      leftSelected: null,
+    }
+  },
+  computed: {
+    rightItems() {
+      if (this.leftSelected && this.leftSelected.children) {
+        return this.leftSelected.children
+      } else {
+        return null
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import './var';
+  .cascaderItem {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    height: 100px;
+    .left {
+      height: 100%;
+      padding: .3em 0;
+    }
+    .right {
+      height: 100%;
+      border-left: 1px solid $border-color-light;
+    }
+    .label {
+      padding: .3em 1em;
+      display: flex;
+      align-items: center;
+      .icon {
+        margin-left: 1em;
+        transform: scale(0.5);
+      }
+    }
+  }
+</style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -4,73 +4,31 @@
       <slot></slot>
     </div>
     <div class="popover" v-if="popoverVisible">
-      <div class="level1">
-        <div
-          class="label"
-          v-for="(item1, index) in source"
-          :key="index"
-          @click="level1Selected = item1"
-        >
-          {{ item1.name }}
-        </div>
-      </div>
-      <div class="level2">
-        <div
-          class="label"
-          v-for="(item2, index) in level2Items"
-          :key="index"
-          @click="level2Selected = item2"
-        >
-          {{ item2.name }}
-        </div>
-      </div>
-      <div class="level3">
-        <div class="label" v-for="(item3, index) in level3Items" :key="index">
-          {{ item3.name }}
-        </div>
-      </div>
+      <cascader-items :items="source" />
     </div>
   </div>
 </template>
 
 <script>
-import CascaderItem from "./cascaderItem.vue";
+ import CascaderItems from './cascader-items'
 export default {
-  name: "GuluCascader",
+  name: 'GuluCascader',
+  components: { CascaderItems },
   data() {
     return {
       popoverVisible: false,
-      level1Selected: null,
-      level2Selected: null,
-    };
+    }
   },
   props: {
     source: {
       type: Array,
     },
   },
-  components: { CascaderItem },
-  computed: {
-    level2Items() {
-      if (this.level1Selected) {
-        return this.level1Selected.children;
-      } else {
-        return [];
-      }
-    },
-    level3Items() {
-      if (this.level2Selected) {
-        return this.level2Selected.children;
-      } else {
-        return [];
-      }
-    },
-  },
-};
+}
 </script>
 
 <style lang="scss" scoped>
-@import "./var";
+@import './var';
 .cascader {
   .trigger {
     border: 1px solid red;
@@ -78,21 +36,11 @@ export default {
     width: 100px;
   }
   .popover {
-    border: 2px solid red;
+    border: 2px solid green;
     height: 200px;
-    width: 80px;
-    position: relative;
-    .level1 {
-      .level2 {
-        position: absolute;
-        left: 100%;
-        top: 0;
-        width: 100%;
-        display: none;
-      }
-      &:hover .level2 {
-        display: block;
-      }
+    display: flex;
+    .label {
+      white-space: nowrap;
     }
   }
 }
